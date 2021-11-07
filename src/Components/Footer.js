df02833582ae9ad9f4cb11e939fb4748eaee9341
Parthasai <53685926+propardhu@users.noreply.github.com>
2021-11-07 19:04:30 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,26 +1,33 @@
 import React, { Component } from "react";
 import Fade from "react-reveal";
+import { BsGithub, BsLinkedin, BsWhatsapp } from "react-icons/bs";
 
 class Footer extends Component {
   render() {
     if (!this.props.data) return null;
-
-    const networks = this.props.data.social.map(function (network) {
-      return (
-        <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
 
     return (
       <footer>
         <div className="row">
           <Fade bottom>
             <div className="twelve columns">
-              <ul className="social-links">{networks}</ul>
+              <ul className="social-links">
+                <li>
+                  <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/partha-sai/">
+                    <BsLinkedin />
+                  </a>
+                </li>
+                <li>
+                  <a target="_blank" rel="noreferrer" href="https://github.com/propardhu">
+                    <BsGithub />
+                  </a>
+                </li>
+                <li>
+                  <a target="_blank" rel="noreferrer" href="https://wa.me/+918500541540?text=Hi">
+                    <BsWhatsapp />
+                  </a>
+                </li>
+              </ul>
             </div>
           </Fade>
 
